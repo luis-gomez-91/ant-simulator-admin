ef0078a92e0b793bd65ff7b169d3edc786b29c15
luisGomez <luis.gomez.veloz@gmail.com>
2025-06-08 00:06:31 -0500

    server url change

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var idLicencia = 1
-// const SERVER_URL = 'https://antsimulator.onrender.com/'
-const SERVER_URL = 'http://127.0.0.1:8000/'
+const SERVER_URL = 'https://antsimulator.onrender.com/'
+// const SERVER_URL = 'http://127.0.0.1:8000/'
 
 async function fetchQuestions() {
     try {
